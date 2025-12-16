b14431f77421a9c7e610bce6c2f2440637a943c0
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-12-16 15:52:22 +0100

    feat: import & export

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
 import express from "express";
-// import { calciatori } from "./data";
-const calciatori = [
-    {
-        id: 1,
-        nome: "Cristiano Ronaldo",
-        img: "./img/cristianoronaldo.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 2,
-        nome: "Lionel Messi",
-        img: "./img/lionelmessi.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 3,
-        nome: "Scott Mc Tominay",
-        img: "./img/scottmctominay.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 4,
-        nome: "Neymar",
-        img: "./img/neymar.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 5,
-        nome: "Eden Hazard",
-        img: "./img/edenhazard.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 6,
-        nome: "Marek Hamsik",
-        img: "./img/marekhamsik.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-    {
-        id: 7,
-        nome: "Edinson Cavani",
-        img: "./img/edinsoncavani.jpg",
-        tags: ["colpo di testa", "attaccante"],
-    },
-]
+import {calciatori} from "./data.js";
 
 const app = express();
 const port = 3000;
--- data.js
+++ data.js
@@ -36,4 +36,4 @@ const calciatori = [
     },
 ]
 
-export { calciatori }
+export {calciatori} 
